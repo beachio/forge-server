70aa83624b9c192fdf90808a10b0dad9c74f6618
molefrog <molefrog@gmail.com>
2016-05-11 19:51:23 +0300

    Add example config file.

---
+++ config.example.js
@@ -0,0 +1,10 @@
+
+exports.config = function() {
+  return {
+    key:    'AKIAJJTVNI4PCPFAENCA',
+    secret: 'TkXefLcpZkERTwHKdBj3ByAm/r3WrkdfTkBi3qqZ',
+    bucket: 'forgemolefrog',
+    region: 'us-west-2',
+    port:   8080
+  };
+}
