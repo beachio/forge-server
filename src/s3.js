18ed04742a2f758ac7420acc0d0f025557cc8285
molefrog <molefrog@gmail.com>
2016-05-15 15:53:36 +0300

    Moved S3 utils into a separate module.

---
+++ src/s3.js
@@ -0,0 +1,37 @@
+const { config } = require('./env')
+const knox       = require('knox')
+
+const s3 = knox.createClient({
+  key:    config.key,
+  secret: config.secret,
+  bucket: config.bucket,
+  region: config.region
+})
+
+exports.client = s3
+
+/*
+ * Loads file content from S3
+ * returns null when no file is found
+ */
+const streamBuffers = require('stream-buffers')
+
+exports.getFileContent = (filepath, cb) => {
+  s3.get(filepath)
+  .on('response', (response) => {
+
+    logger(response)
+
+    if(response.statusCode != 200)
+      return cb(null, null)
+
+    let buffer = new streamBuffers.WritableStreamBuffer
+    response.pipe(buffer)
+
+    response.on('end', () => {
+      let content = buffer.getContentsAsString('utf8')
+      cb(null, content)
+    })
+
+  }).end()
+}
