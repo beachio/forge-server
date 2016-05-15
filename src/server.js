5cb78527ba1e2b8409ce35480af5c1d3f9ef7dac
molefrog <molefrog@gmail.com>
2016-05-15 16:04:05 +0300

    server.js: cleaned up logger middleware.

--- src/server.js
+++ src/server.js
@@ -1,17 +1,15 @@
-const { config }  = require('./env')
 const express     = require('express')
+
+const { config }  = require('./env')
+const logger      = require('./logger')
 
 let app = express()
 
 app.use(require('./middlewares/common'))
-app.use((req, res, next) => {
-  console.log(`⚓️  ${req.context.address} ${req.context.path}`)
-  next()
-})
 app.use(require('./middlewares/rules'))
 app.use(require('./middlewares/static'))
 app.use(require('./middlewares/notFound'))
 
 app.listen(config.port, function () {
-  console.log('🚀  Proxy running...');
+  logger(`🚀  Proxy running on 0.0.0.0:${config.port}`)
 })
