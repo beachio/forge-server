06b7f6a4ac9d1b7c16d289bba12b592be032fb16
Marina Pecherina <lumandra7@gmail.com>
2022-07-15 10:21:20 +0300

    Added check on empty rules

--- src/middlewares/common.js
+++ src/middlewares/common.js
@@ -90,7 +90,7 @@ const loadSiteMeta = (address, done) => {
       let meta = {}
       try { meta = JSON.parse(metaContent) } catch(err) { success = false }
 
-      if(success) {
+      if(success && meta.configRaw.trim().length > 0) {
         logger(`✅  Meta for site loaded from ${metaFileLocation}`)
         return done(null, meta)
       }
